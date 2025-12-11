2ea3fad37091d329a788eb8cf5fcaabd9ed131a6
Fathan Rohman <karir.fathan@gmail.com>
2025-12-11 07:13:27 +0700

    fix: routes API

--- src/app/api/quran/[surah]/route.ts
+++ src/app/api/quran/[surah]/route.ts
@@ -2,12 +2,19 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 
-export async function GET(req: Request, context: { params: { surah: string } }) {
-  const { params } = await context;
-  const { surah } = await params;
+export async function GET(
+  req: Request,
+  context: { params: Promise<{ surah: string }> }
+) {
+  const { surah } = await context.params;
 
   try {
-    const filePath = path.join(process.cwd(), "src/database/surah", `${surah}.json`);
+    const filePath = path.join(
+      process.cwd(),
+      "src/database/surah",
+      `${surah}.json`
+    );
+
     const file = fs.readFileSync(filePath, "utf8");
     const json = JSON.parse(file);
 
