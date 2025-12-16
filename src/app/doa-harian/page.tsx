d360ace613adf7e10f26b32e78fd9816de8138b4
fathan <karir.fathan@gmail.com>
2025-12-16 14:16:31 +0700

    updated style

--- src/app/doa-harian/page.tsx
+++ src/app/doa-harian/page.tsx
@@ -27,10 +27,10 @@ export default async function DoaHarian () {
   return (
     <div className="p-6 space-y-6">
       {data.doaList.map((doa: IDoa, index: number) => (
-        <div key={index} className="border pb-4 bg-gray-100 rounded-lg shadow-md p-4">
+        <div key={index} className="border pb-4 bg-white rounded-lg p-4">
           <h2 className="text-xl font-semibold mb-10">{doa.title}</h2>
-          <p className="text-3xl mb-2 font-extralight">{doa.text}</p>
-          <p className="mb-2">{doa.translation}</p>
+          <p className="text-4xl mb-5 font-extralight">{doa.text}</p>
+          <p className="mb-2 italic">Artinya: {doa.translation}</p>
         </div>
       ))}
 
--- src/components/fragments/alquran/organisms/AyahItem/index.tsx
+++ src/components/fragments/alquran/organisms/AyahItem/index.tsx
@@ -12,8 +12,8 @@ const AyahItem = async (props: IProps) => {
   return (
     <div className="bg-white rounded-lg p-4 border border-gray-200 flex flex-col gap-4">
       <div className="flex flex-col gap-4">
-        <div className="flex justify-between items-center">
-          <div className="font-semibold text-base">
+        <div className="flex justify-between items-center mb-7">
+          <div className="font-semibold text-base rounded-full w-12 h-12 flex items-center justify-center bg-gray-200">
             {number}
           </div>
           <div className="font-light text-sm">
@@ -21,15 +21,15 @@ const AyahItem = async (props: IProps) => {
           </div>
         </div>
 
-        <div className="flex flex-col gap-4 font-arabic text-2xl">
+        <div className="flex flex-col gap-4 font-arabic text-4xl mb-7">
           <div className="text-right">
             {text}
           </div>
         </div>
 
         <div className="flex justify-end">
-          <div className="font-light text-sm italic">
-            {translation}
+          <div className="font-light text-base italic">
+            <span className="font-semibold">Artinya:</span> {translation}
           </div>
         </div>
       </div>
